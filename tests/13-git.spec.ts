6df1817ae7da158776ee05ab6a21a86dde3590c1
Bikram Panda <bikramkpanda@gmail.com>
2026-09-02 10:55:43 +0530

    added a new test 13-git.spec.ts for learning from git session

---
+++ tests/13-git.spec.ts
@@ -0,0 +1,21 @@
+/* Following are steps or commands to work on git for playwright
+
+1. Install git
+2. Create a repo in github 
+3. from vs code - go to project you working on 
+	git init
+	git add .
+	git commit -m "message"
+
+Incase if you get error perform below
+	git config --global user.name "Bikram Panda" [provide github username]
+	git config --global user.email bikramkpanda@gmail.com [github registered email] 
+
+	git remote add origin https://github.com/bikrampanda85/Playwright_78Batch.git  [Add your git repo URL]
+	git branch -M main
+	git push -u origin main
+	
+	
+4. go to youe github repo and verify that the files are uploaded
+
+*/
